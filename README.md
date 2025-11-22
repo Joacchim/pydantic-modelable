3c4dffd37dc69fdb512ca047dc4a6d0f58a86b02
David Pineau <david.pineau@shadow.tech>
2025-11-22 15:38:24 +0100

    Setup project, CI & first feature
    Yeah it's a big commit, but who cares ?!
    Much simpler to commit everything at once here at this stage.

--- README.md
+++ README.md
@@ -1,2 +1,40 @@
 # pydantic-modelable
-A set of utilities around pydantic that allows to create extensible pydantic models, with little code, in an aim to have models extended by third-party python code.
+
+A set of utilities around pydantic that allows to create extensible pydantic
+models, with little code, in an aim to have models extended by third-party
+python code.
+
+
+## Features
+
+Using `pydantic` for type modelisation and validation has become a very common
+practice. That being said, some advanced uses are not natively supported,
+although the pydantic types are extremely flexible, such as dynamic
+extensibility of the models.
+
+It can be very useful to define extensible models relying on this mechanism,
+and `pydantic_modelable`, as it may provide the following benefits:
+ - Reduction of code maintenance (defining an "extension" registers it
+   automatically wherever the base was setup)
+ - Easy extension of a core library's models and features through the loading
+   of extension modules
+ - Automatically updated Model schemas for inclusion in any schema-based
+   tooling or framework (ex: FastAPI's OpenAPI Schema generation tooling)
+
+With a few additional parameters to your model's constructor, inheriting from
+`pydantic_modelable.Modelable`, you can thus configure various behaviors for
+your extensible model:
+ - discriminated union: `discriminator=attr_name`
+ -
+
+You can then register other models into your base model using decorators
+embedded into your base model by the `pydantic_modelable.Modelable` class:
+ - `extends_enum`
+ - `extends_union(dicriminated_union_attr_name: str)`
+
+
+## Usages of `pydantic_modelable`
+
+The following documents will describe the various uses of `pydantic_modelable`:
+
+ - [Discriminated unions](./docs/examples/discriminated_union.md)
---
+++ pydantic_modelable/mixins.py
@@ -0,0 +1,81 @@
+"""Mixins classes for using pydantic_modelable."""
+
+from typing import Any, ClassVar, cast
+
+import aenum
+from pydantic import (
+    GetCoreSchemaHandler,
+    GetJsonSchemaHandler,
+)
+from pydantic.json_schema import JsonSchemaValue
+from pydantic_core import CoreSchema, core_schema
+
+
+class ModelableEnumMixin:
+    """A mixin allowing to define a `aenum`-base Enum.
+
+    Enum is extended using the defined discriminator values of a Modelable
+    discriminated union model. To define such an extendable enum, one may
+    write:
+    ```py
+    from pydantic_modelable.enum import StrEnumMixin
+
+    class ExampleEnum1(StrEnumMixin, str, aenum.Enum):
+        ...
+    ```
+    """
+
+    __schema_title__: ClassVar[str] = ''
+    __schema_description__: ClassVar[str] = ''
+
+    __setup__: ClassVar[bool] = False
+
+    def __init_subclass__(cls, schema_title: str = '', schema_description: str = '', **kwargs: Any) -> None:
+        """Initialize the ModelableEnumMixin with subclass custom parameters.
+
+        Parameters are used to customize the Schema model to be generated
+        (description, title, etc).
+        """
+        super().__init_subclass__(**kwargs)
+        cls.__schema_title__ = schema_title if schema_title else f'{cls.__name__}'
+        cls.__schema_description__ = schema_description if schema_description else f'Extensible enum for {cls.__name__}'
+
+    @classmethod
+    def _add_choice(cls, choice: str) -> None:
+        """Add a new option into the Enum."""
+        # Ensure `_pydantic_modelable_invalid_` member does not remain
+        if not cls.__setup__:
+            # Field comes from aenum.Enum, so type-checking does not complain
+            # if we cast to that type, given we "ignore" that typing import
+            cast(aenum.Enum, cls)._member_names_ = []
+            cls.__setup__ = True
+        # Now we can extend the list of available values
+        aenum.extend_enum(cls, choice, choice)
+
+    @classmethod
+    def __get_pydantic_core_schema__(cls, source: type[Any], handler: GetCoreSchemaHandler) -> core_schema.CoreSchema:
+        """Redefine the pydantic core schema using the dynamically injected Enum values."""
+        return core_schema.enum_schema(
+            cls,
+            # We need to provide at least one "choice" for an enum schema.
+            # Additionally, mypy complains on the Type having no "__iter__"
+            # attribute but it works nonetheless
+            (['none'] if not cls.__setup__ else list(cls._member_map_.values())),  # type: ignore
+            sub_type='str',
+            strict=True,
+        )
+
+    @classmethod
+    def __get_pydantic_json_schema__(
+        cls,
+        _core_schema: CoreSchema,
+        _handler: GetJsonSchemaHandler,
+    ) -> JsonSchemaValue:
+        """Redefine the pydantic JSON schema using the dynamically injected Enum values."""
+        return {
+            # NOTE(joa) aenum is not type-hinted: mypy can't find __iter__ for aenum.Enum
+            'enum': [m.name for m in cls],  # type: ignore
+            'type': 'string',
+            'title': cls.__schema_title__,
+            'description': cls.__schema_description__,
+        }
